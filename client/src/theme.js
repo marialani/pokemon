e37b924fa1a337eefcca2b036fafdf1daf82680e
Maria Olanipekun <m.olanipekun@hotmail.com>
2020-06-29 14:09:42 +0100

    create material ui theme

---
+++ client/src/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+// import black from "@material-ui/core/colors/black";
+
+export default createMuiTheme({
+  // Customize MU theme properties here;
+  // check default theme here https://material-ui.com/customization/default-theme/#default-theme;
+  palette: {
+    primary: {
+      main: "#353839",
+    },
+  },
+});
